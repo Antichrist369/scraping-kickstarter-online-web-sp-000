2d3b80e4a887d8095f8d613453d7a82ccc2f2152
Anthony Molina <antonio-molina@att.net>
2019-09-04 05:35:40 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,7 +18,7 @@ def create_project_hash
    project.css("p.bbcard_blurb").text
    project.css("ul.project-meta span.location-name").text 
    project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i 
-   }
+    }
     
   end 
   
