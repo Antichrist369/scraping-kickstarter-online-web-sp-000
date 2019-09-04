625ffec4754aed2d4dc559599536700aeb5e48b2
Anthony Molina <antonio-molina@att.net>
2019-09-04 05:37:02 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,7 +14,7 @@ def create_project_hash
     
     projects[title.to_sym] = { 
     
-   project.css("div.project-thumbnauil a img").attribute("src").value
+   :image_link => project.css("div.project-thumbnauil a img").attribute("src").value
    project.css("p.bbcard_blurb").text
    project.css("ul.project-meta span.location-name").text 
    project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i 
