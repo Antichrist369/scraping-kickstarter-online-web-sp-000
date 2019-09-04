8eac3107760e4a433b5861da78b0ce46f1e4ebe7
Anthony Molina <antonio-molina@att.net>
2019-09-04 05:15:42 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,6 +14,8 @@ def create_project_hash
     
     projects[title.to_sym] = {} 
     
+    
+    
   end 
   
   projects
@@ -22,7 +24,7 @@ end
 create_project_hash 
 #projects: kickstarter.css("li.project.grid_4")
 #title: project.css("h2.bbcard_name strong a").text
-#project.css("div.project-thumbnauil a img").attribute("src").value 
+# 
 #description: project.css("p.bbcard_blurb").text
 #location: project.css("ul.project-meta span.location-name").text 
 #percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i 
